3dc12c3904492119e3c464807f5b8e05789c3f6c
Carlos Garces <CGarces@users.noreply.github.com>
2020-11-24 10:47:37 +0100

    Fix java package name on Swagger configuration and docker file.

--- routes_service/src/main/java/com/routestest/routesservice/config/SwaggerConfig.java
+++ routes_service/src/main/java/com/routestest/routesservice/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2).select()
-        .apis(RequestHandlerSelectors.basePackage("com.routes_test.routes_service.rest"))
+        .apis(RequestHandlerSelectors.basePackage("com.routestest.routesservice.rest"))
         .paths(PathSelectors.any())
         .build();
   }
